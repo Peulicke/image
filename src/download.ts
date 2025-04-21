660a6ec5a9805314c069d729e2fe5887a78eb6f4
Oscar <oscarrpeulicke@gmail.com>
2025-04-21 12:16:40 +0200

    first commit

---
+++ src/download.ts
@@ -0,0 +1,28 @@
+const imageToBlob = (image: HTMLImageElement): Promise<Blob> =>
+    new Promise((resolve, reject) => {
+        const canvas = document.createElement("canvas");
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+
+        const ctx = canvas.getContext("2d");
+        ctx?.drawImage(image, 0, 0);
+
+        canvas.toBlob(blob => {
+            if (blob === null) reject();
+            else resolve(blob);
+        }, "image/png");
+    });
+
+const downloadBlob = (blob: Blob, fileName: string): void => {
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
+export const downloadImage = async (image: HTMLImageElement, fileName: string): Promise<void> => {
+    const blob = await imageToBlob(image);
+    downloadBlob(blob, fileName);
+};
